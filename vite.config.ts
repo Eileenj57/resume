2a148e0935aec3aa30681aa85ca46cfd8eae85fa
Eileen <jovenineileen@gmail.com>
2026-02-16 13:36:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/interactive-resume-template/',
+  base: '/resume/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
